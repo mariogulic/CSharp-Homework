c0b314cbc155ffc53eb1f66be9c35d6879a38477
mariogulic <153008070+mariogulic@users.noreply.github.com>
2024-03-21 16:17:27 +0100

    Add files via upload

---
+++ ArrayOfIntegers/ArrayOfIntegers/Program.cs
@@ -0,0 +1,16 @@
+﻿//Declare a new array of integers with 5 elements
+//Initialize the array elements with values from input
+//Sum all the values and print the result in the console
+
+int[] integerArray = new int[5];
+int sumOfNumbers = 0;
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Enter number for the array:");
+    integerArray[i] = int.Parse(Console.ReadLine());
+}
+for (int i = 0; i < integerArray.Length; i++)
+{
+    sumOfNumbers += integerArray[i];
+}
+Console.WriteLine(sumOfNumbers);
---
+++ ArrayOfNames/ArrayOfNames/Program.cs
@@ -0,0 +1,28 @@
+﻿//Create an array with names
+//Give an option to the user to enter a name from the keyboard
+//After entering the name put it in the array
+//Ask the user if they want to enter another name (Y/N)
+//Do the same process over and over untill the user enter N
+//Print all the names after user enters N   
+
+string[] arrayNames = new string[0];
+int index = 0;
+
+while (true)
+{
+    Console.WriteLine("Enter a name (or 'N' to stop):");
+    string input = Console.ReadLine();
+
+    if (input == "N")
+        break;
+
+    Array.Resize(ref arrayNames, index + 1);
+    arrayNames[index] = input;
+    index++;
+}
+
+Console.WriteLine("Names entered:");
+foreach (string name in arrayNames)
+{
+    Console.WriteLine(name);
+}
