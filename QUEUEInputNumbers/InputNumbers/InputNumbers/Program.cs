c94f7f5c4eed5fafe6cd6ff868198880d168ecdc
mariogulic <153008070+mariogulic@users.noreply.github.com>
2024-03-26 13:12:18 +0100

    Add files via upload

---
+++ QUEUEInputNumbers/InputNumbers/InputNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿Queue<int> numbersQueue = new Queue<int>();
+while (true)
+{
+    Console.WriteLine("Please enter number: ");
+    int.TryParse(Console.ReadLine(), out int numberInput);
+    numbersQueue.Enqueue(numberInput);
+    Console.WriteLine("Would you like to continue: ");
+    string continueAnswer = Console.ReadLine();
+    if (continueAnswer.ToLower() == "yes")
+    {
+        continue;
+    }
+    else
+    {
+        break;
+    }
+
+}
+foreach (int number in numbersQueue)
+{
+    Console.WriteLine(number);
+}
